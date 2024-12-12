62cdb2ebe244dee14982c1a335196c7aa030b01b
Alessandro Aiezza <a.aiezza@gmail.com>
2024-12-12 03:38:40 -0500

    improve: alter schema

--- db/migration/V1__initial_schema.sql
+++ db/migration/V1__initial_schema.sql
@@ -2,16 +2,20 @@
 
 -- Create Customer Table
 CREATE TABLE customer (
-  customer_id UUID PRIMARY KEY,
+  customer_dbid BIGINT GENERATED ALWAYS AS IDENTITY,
+  customer_id UUID NOT NULL,
   full_name VARCHAR(150) NOT NULL,
   preferred_name VARCHAR(50) NOT NULL,
-  email_address VARCHAR(100) UNIQUE NOT NULL,
+  email_address VARCHAR(100) NOT NULL,
   phone_number VARCHAR(15) NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
   deleted_at TIMESTAMP WITH TIME ZONE
+  CONSTRAINT pk_customer_dbid PRIMARY KEY (customer_dbid),
+  CONSTRAINT ux_customer_id UNIQUE (customer_id)
+  CONSTRAINT ux_customer_email_address UNIQUE (email_address)
 );
 
 -- Indexes for performance
-CREATE INDEX idx_customer_email ON customer (email_address);
-CREATE INDEX idx_customer_phone ON customer (phone_number);
+CREATE INDEX ux_customer_email ON customer (email_address);
+CREATE INDEX ix_customer_phone ON customer (phone_number);
